ab75e5bc0b7bf659073f559b981dfdfdb4284902
Tyler Hagstrom <tyler.hagstrom@gmail.com>
2022-01-14 11:14:22 -0600

    Add sql connection

--- InventoryTracker/Program.cs
+++ InventoryTracker/Program.cs
@@ -1,19 +1,21 @@
+using Microsoft.EntityFrameworkCore;
+using InventoryTracker.Models;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
+builder.Services.AddDbContext<InventoryContext>(opt =>
+        opt.UseSqlServer("Server=(localdb)\\MSSQLLocalDB; Initial Catalog=InventoryDB; Integrated Security=true;")
+        );
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseDeveloperExceptionPage();
 }
 
 app.UseHttpsRedirection();
